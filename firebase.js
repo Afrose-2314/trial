2e4b2782b84cad0c6aaabf8e72dbc673f9ceb645
Afrose <12851bca2023@princescience.in>
2025-12-10 19:48:28 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js"></script>
+
+<script>
+const firebaseConfig = {
+    apiKey: "AIzaSyArVVndFLxAfL6tbnEOKf4BaZ_nMbOwJiU",
+    authDomain: "python-coding-90ac7.firebaseapp.com",
+    databaseURL: "https://python-coding-90ac7-default-rtdb.firebaseio.com",
+    projectId: "python-coding-90ac7",
+    storageBucket: "python-coding-90ac7.firebasestorage.app",
+    messagingSenderId: "226893634662",
+    appId: "1:226893634662:web:182989842cf1906902e1a7"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+</script>
