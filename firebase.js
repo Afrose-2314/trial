e91e14db297013282a8471c40f80d7157028aff5
Afrose <12851bca2023@princescience.in>
2025-12-10 20:05:02 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
-<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js"></script>
+// Firebase App (the core Firebase SDK)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, set, get, onValue, update } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-<script>
+// Firebase config
 const firebaseConfig = {
     apiKey: "AIzaSyArVVndFLxAfL6tbnEOKf4BaZ_nMbOwJiU",
     authDomain: "python-coding-90ac7.firebaseapp.com",
@@ -12,6 +13,7 @@ const firebaseConfig = {
     appId: "1:226893634662:web:182989842cf1906902e1a7"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-</script>
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export { ref, set, get, onValue, update };
